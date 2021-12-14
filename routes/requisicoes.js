1e777f6da5df25d7195683bfcd53985f373a70b8
Maxohl <maxohl@hotmail.com>
2021-12-14 09:56:55 -0300

    testing

--- routes/requisicoes.js
+++ routes/requisicoes.js
@@ -8,7 +8,6 @@ const isLoggedIn = require('../utils/isLogged');
 const moment = require('moment');
 const converte = require('../utils/convertDate');
 const con = require('../utils/pool');
-const handleDisconnect = require('../utils/handleDisconnect');
 // const con = require('../utils/connection')
 require('dotenv').config();
 
