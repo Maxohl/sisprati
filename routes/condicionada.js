cd79b03e1ebc5e4e0e66c0051e233f4dc7399676
Maxohl <maxohl@hotmail.com>
2021-12-24 09:49:34 -0300

    added code to send mail in condionada

--- routes/condicionada.js
+++ routes/condicionada.js
@@ -11,6 +11,83 @@ require('dotenv').config();
 
 let copia;
 let nameShip;
+let lista = [];
+
+//Email para requisicoes condicionadas
+const mailCondi = {
+
+    Subject: '',
+    Navio: '',
+    Data: '',
+    Servico:'',
+    Berco: '',
+    Posicao:'',
+    IMO:'',
+    Bandeira:'',
+    Armador:'',
+    Carga:'',
+    GRT:'',
+    DWT:'',
+    LOA:'',
+    Entrada: '',
+    Saida: '',
+    Faturamento: '',
+    Obs : '',   
+}
+
+//Pega lista de e-mails para envio e adiciona eles para variavel lista
+function mailList(){
+    const query = 'SELECT email FROM emails';
+    con.query(query,function(err,result,fields){ 
+        for(let i = 0; i < result.length; i++){
+           lista.push(result[i].email);
+        }
+    })
+    }
+
+//Envia e-mail de requisicao condicionada
+function sendCondi(cookie){
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    let situacao;
+    if(mailCondi.Servico =='DESATRACACAO' || 'DESATRACACAOF'){
+        situacao = `<b>CALADO DE SAÍDA: </b>${mailCondi.Saida}<br>`;    
+    }else{
+        situacao = `<b>CALADO DE ENTRADA: </b>${mailCondi.Entrada}<br>`;
+    }
+    //conteudo do e-mail
+    const msg = {
+      to: [lista], // Change to your recipient
+      from: 'sisprati@hotmail.com', // Change to your verified sender
+      subject: `${mailCondi.Subject} ${mailCondi.Navio}`,
+      text: 'Teste',
+      html:
+           `<h1><b>${cookie}</b></h1><br>
+           <b>NAVIO:${mailCondi.Navio}</b><br>
+           <b>DATA:</b> ${mailCondi.Data}<br>
+           <b>SERVIÇO:</b> ${mailCondi.Servico}<br>
+           <b>BERÇO:</b> ${mailCondi.Berco}<br>
+           <b>POSIÇÃO:</b> ${mailCondi.Posicao}<br>
+           <b>IMO:</b> ${mailCondi.IMO}<br>
+           <b>BANDEIRA:</b> ${mailCondi.Bandeira}<br>
+           <b>ARMADOR:</b> ${mailCondi.Armador}<br>
+           <b>CARGA:</b> ${mailCondi.Carga}<br>
+           <b>GROSS/TBR:</b> ${mailCondi.GRT}<br>
+           <b>DWT:</b> ${mailCondi.DWT}<br>
+           <b>LOA:</b> ${mailCondi.LOA}<br>
+           ${situacao}
+           <b>FATURAMENTO:</b> ${mailCondi.Faturamento}<br>
+           <b>OBS/CONTATOS:</b> ${mailCondi.Obs}`
+    }
+    sgMail
+      .sendMultiple(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    }    
 
 const validateCondi = (req,res,next) => {
     const { error } = condicionadaSchema.validate(req.body);
@@ -61,6 +138,7 @@ function allRequi(req){
 }
 
 router.get('/', isLoggedIn,(req,res) => {
+    mailList();
     allRequi(req); 
     const navios = `SELECT * FROM navios where ID_agencia = ${req.user.ID_agencia}`;
     con.query(navios,function(err,result,fields){
@@ -78,6 +156,15 @@ router.post('/',validateCondi, isLoggedIn,catchAsync(async(req,res) => {
     con.query(condicionada,function(err,result,fields){
         if(err) throw(err);
         console.log(result);
+        mailCondi.Subject = 'REQUISIÇÃO DE SERVIÇOS DE PRATICAGEM ';
+        mailCondi.Data = dataRequi + ' ' +caminho.Hora_requi;
+        mailCondi.Berco = caminho.berco_requi;  
+        mailCondi.Servico = caminho.Requi_servico; 
+        mailCondi.Posicao = caminho.posicao_requi; 
+        mailCondi.Faturamento = caminho.Fatu_requi;    
+        mailCondi.Obs = caminho.Obs_requi; 
+        mailCondi.Servico = caminho.Requi_servico;
+        sendCondi(cookie,mailCondi);
         req.flash('Sucesso','Requisição Condicionada registrada com sucesso');
         res.redirect('/navios');
     })
@@ -108,8 +195,25 @@ router.get('/:id', isLoggedIn, catchAsync(async(req,res,next) => {
         const navio = `SELECT * FROM navios WHERE ID = ${result[0].ID_NavioMain}`
         console.log(navio);
         console.log(result[0]);
+        mailCondi.Data = converte(result[0].Data_requi);
+        mailCondi.Berco = result[0].berco_requi;  
+        mailCondi.Servico = result[0].Requi_servico; 
+        mailCondi.Posicao = result[0].posicao_requi; 
+        mailCondi.Faturamento = result[0].Fatu_requi;    
+        mailCondi.Obs = result[0].Obs_requi; 
+        mailCondi.Servico = result[0].Requi_servico; 
         con.query(navio, function(err,rows,fields){
             const nome = rows[0].Navio;
+            mailCondi.Navio = rows[0].Navio;
+            mailCondi.IMO = rows[0].IMO;
+            mailCondi.Bandeira = rows[0].Bandeira;
+            mailCondi.Armador = rows[0].armador;
+            mailCondi.Carga = rows[0].Carga;
+            mailCondi.GRT = rows[0].GRT;
+            mailCondi.DWT = rows[0].DWT;
+            mailCondi.LOA = rows[0].LOA;
+            mailCondi.Entrada = 'FWD: '+result[0].C_proa+'m  AFT: '+rows[0].C_popa+'m';
+            mailCondi.Saida = 'FWD: '+result[0].CS_proa+'m AFT: '+rows[0].CS_popa+'m';
             res.render('condicionada/show',{Navio:nome, requiData:result})
         })
       }else{
@@ -119,12 +223,22 @@ router.get('/:id', isLoggedIn, catchAsync(async(req,res,next) => {
 }));
 
    router.put('/:id', validateCondi,catchAsync(async(req,res, next) => {
+    const cookie = req.cookies.ID;
     const caminho = req.body.condi;
     const dataCondi = converte(caminho.Data);
     const sql = `UPDATE Condicionado SET ID_NavioMain="${caminho.ID_NavioMain}", ID_NavioSub="${caminho.ID_NavioSub}", Data="${dataCondi}", Viagem="${caminho.Viagem}", Servico="${caminho.Servico}", Berco="${caminho.Berco}", Posicao_Berco="${caminho.Posicao_Berco}", OBS="${caminho.OBS}", Fatu="${caminho.Fatu}", ID_Agencia="${req.user.ID_agencia}" WHERE ID = ${caminho.ID}`;
     console.log(sql);
     con.query(sql,function(err,result,fields){
         if(err) throw(err);
+        mailCondi.Subject = 'ATUALIZAÇÃO DE REQUISIÇÃO CONDICIONADA DE SERVIÇOS DE PRATICAGEM' 
+        mailCondi.Data = dataCondi;
+        mailCondi.Berco = caminho.Berco;  
+        mailCondi.Servico = caminho.Servico; 
+        mailCondi.Posicao = caminho.Posicao_Berco; 
+        mailCondi.Faturamento = caminho.Fatu;    
+        mailCondi.Obs = caminho.OBS; 
+                       
+        sendCondi(cookie,mailCondi);
         console.log(result);
         req.flash('Sucesso','Requisição Condicionada Atualizada com sucesso!');
         res.redirect('/navios');
@@ -150,6 +264,8 @@ router.delete('/:id',catchAsync(async(req,res,next) => {
     con.query(sql,function(err,result){
         if(err) throw(err)
         console.log('Number of records deleted:'+result.affectedRows);
+        mailCondi.Subject = 'CANCELAMENTO DE REQUISIÇÃO CONDICIONADA DE SERVIÇOS DE PRATICAGEM' 
+        sendCondi(cookie,mailCondi);
         req.flash('Sucesso','Requisição Condicionada Cancelada!')
         res.redirect('/requisicoes');
     })
