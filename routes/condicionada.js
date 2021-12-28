f12dd7c2c55f09dae353444c9c8abd8be1604f16
Maxohl <maxohl@hotmail.com>
2021-12-28 09:18:15 -0300

    I hate e-mails

--- routes/condicionada.js
+++ routes/condicionada.js
@@ -60,9 +60,9 @@ function sendCondi(cookie){
     //conteudo do e-mail
     const msg = {
        // Change to your recipient
-    //   to: 'maxohl@hotmail.com',
+      to: 'maxohl@hotmail.com',
       from: 'sisprati@hotmail.com', // Change to your verified sender
-      to: [lista],
+    //   to: [lista],
       subject: `${mailCondi.Subject} ${mailCondi.Navio}`,
       text: 'Teste',
       html:
