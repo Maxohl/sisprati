5ec8219f3a91c92b608b77d9a61e2beb5e7cbf32
Maxohl <maxohl@hotmail.com>
2021-12-28 13:45:26 -0300

    teste

--- routes/requisicoes.js
+++ routes/requisicoes.js
@@ -99,7 +99,47 @@ const msg = {
   from: {
       email: 'sisprati@hotmail.com'
   },
-  to: listEmail,
+  to: [
+    {
+        email: 'maxohl@hotmail.com',
+     },
+     {
+         email: 'maxohl12@gmail.com',
+     },
+     {
+         email: 'vtsvtsibb@gmail.com',
+     },
+     {
+         email: 'thiagolealrj@yahoo.com.br',
+     },
+     {
+         email: 'raphaelinglez@gmail.com',
+     },
+     {
+         email: 'imbitubapilots@gmail.com',
+     },
+     {
+         email: 'rduarte45@gmail.com',
+     },
+     {
+         email: 'lgbazilio@gmail.com',
+     },
+     {
+         email: 'operacoes@portodeimbituba.com.br',
+     },
+     {
+         email: 'Elton.villa@hotmail.com',
+     },
+     {
+         email: 'veduardo618@gmail.com',
+     },
+     {
+         email: 'navegacaovilla@gmail.com',
+     },
+     {
+         email: 'eltonvillajr@gmail.com',
+     },
+  ],
   subject: `${mailLetter.Subject} ${mailLetter.Navio}`,
   text: 'Teste',
   html:
