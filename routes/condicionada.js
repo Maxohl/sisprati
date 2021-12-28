b98a7e5c4a2ddc4d00c5be7dcc94dfb299e536ca
Maxohl <maxohl@hotmail.com>
2021-12-28 10:18:04 -0300

    test

--- routes/condicionada.js
+++ routes/condicionada.js
@@ -53,9 +53,20 @@ function sendCondi(cookie){
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     let situacao = `<b>CALADO DE ENTRADA: </b>${mailCondi.Entrada}<br>`;
     //conteudo do e-mail
+    // Create the personalizations object that will be passed to our message object
+    let personalizations = [{
+        to: [],
+        subject
+    }];
+
+    for(let index in lista){
+        personalizations[0].to[index] = {email: lista[index]}
+    };
+
     const msg = {
        // Change to your recipient
-      to: `${[lista]}`,
+    //   to: `${[lista]}`,
+      personalizations,
       cc: 'maxohl@hotmail.com',
       from: 'sisprati@hotmail.com', // Change to your verified sender
     //   to: [lista],
