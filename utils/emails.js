3fd5ccca3733b06f9be36fd6ce54d669fd01fb60
Maxohl <maxohl@hotmail.com>
2021-12-29 08:25:08 -0300

    testing

--- utils/emails.js
+++ utils/emails.js
@@ -5,35 +5,35 @@
      {
          email: 'vtsvtsibb@gmail.com',
      },
-    //  {
-    //      email: 'thiagolealrj@yahoo.com.br',
-    //  },
-    //  {
-    //      email: 'raphaelinglez@gmail.com',
-    //  },
-    //  {
-    //      email: 'imbitubapilots@gmail.com',
-    //  },
-    //  {
-    //      email: 'eltonvillajr@gmail.com',
-    //  },
-    //  {
-    //     email: 'rduarte45@gmail.com',
-    // },
-    // {
-    //     email: 'lgbazilio@gmail.com',
-    // },
-    // {
-    //     email: 'operacoes@portodeimbituba.com.br',
-    // },
-    // {
-    //     email: 'Elton.villa@hotmail.com',
-    // },
-    // {
-    //     email: 'veduardo618@gmail.com',
-    // },
-    // {
-    //     email: 'navegacaovilla@gmail.com',
-    // },
+     {
+         email: 'thiagolealrj@yahoo.com.br',
+     },
+     {
+         email: 'raphaelinglez@gmail.com',
+     },
+     {
+         email: 'imbitubapilots@gmail.com',
+     },
+     {
+         email: 'eltonvillajr@gmail.com',
+     },
+     {
+        email: 'rduarte45@gmail.com',
+    },
+    {
+        email: 'lgbazilio@gmail.com',
+    },
+    {
+        email: 'operacoes@portodeimbituba.com.br',
+    },
+    {
+        email: 'Elton.villa@hotmail.com',
+    },
+    {
+        email: 'veduardo618@gmail.com',
+    },
+    {
+        email: 'navegacaovilla@gmail.com',
+    },
   ]
   module.exports = listEmail;
